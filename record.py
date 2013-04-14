6be0d1b41483ebfd5350e9809a14de82528e7193
happytreefriend <lovelessyuyu353@qq.com>
2013-04-14 14:19:43 +0800

    begin

---
+++ record.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+#email:natsuki@myclover.org
+#script:record.py
+down_URL='http://cctalkoj.hujiang.com/chat/chatHistory.aspx?page=203&search=&bias=-480&senderid=&receiverid=&RoomId=323833323238&key=1132&range=-1&chatId=0'
+
+from BeautifulSoup import *
+from urlparse import *
+
+def get_page_end():
+    pass
+
+def down_chat():
+    pass
