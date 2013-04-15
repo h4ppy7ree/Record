d48654f0f7e603cf1e0a3c1bd37f90c5969239fa
happytreefriend <lovelessyuyu353@qq.com>
2013-04-15 22:50:08 +0800

    end

--- record.py
+++ record.py
@@ -5,8 +5,8 @@
 from BeautifulSoup import *
 import urllib, urllib2, re
 
-base_URL = 'http://cctalkoj.hujiang.com/chat/chatHistory.aspx?page=108&search=&bias=-480&senderid=&receiverid=&RoomId=323833323235&key=780&range=-1&chatId=0'
-#base_URL = 'http://cctalkoj.hujiang.com/chat/chatHistory.aspx?page=203&search=&bias=-480&senderid=&receiverid=&RoomId=323833323238&key=1132&range=-1&chatId=0'
+base_URL = 'http://cctalkoj.hujiang.com/chat/chatHistory.aspx?page=92&search=&bias=-480&senderid=&receiverid=&RoomId=323833323235&key=780&range=-1&chatId=0'
+#base_URL = 'http://cctalkoj.hujiang.com/chat/chatHistory.aspx?page=247&search=&bias=-480&senderid=&receiverid=&RoomId=323833323238&key=1132&range=-%201&chatId=0'
 
 
 #解析html对象
@@ -23,10 +23,10 @@ class Soup():
 #处理URL
 def get_page_value():
     return  re.compile(r'page=(?P<value>\d+)&').findall(base_URL)[0]
-def get_down_url(page):
+def get_down_url(url, page):
     '''当前下载的url'''
     #    global base_PACK
-    return base_URL + '&page=' + str(page)
+    return url + '&page=' + str(page)
 
 #下载页面
 def down_page(url):
@@ -48,9 +48,10 @@ def get_page_end():
 def down_chat():
     save_page('<html>')
     page_end = get_page_end()
+    url = base_URL.replace('page='+str(page_end)+'&', '')
     for i in range(1,page_end + 1):
-        print '正在保存Page' + str(i) + get_down_url(i)
-        soup = Soup(down_page(get_down_url(i)))
+        print '正在保存Page' + str(i) + get_down_url(url, i)
+        soup = Soup(down_page(get_down_url(url, i)))
         if i == 1:
             print page_end
             save_page(soup.get_html_tag('head').prettify() + '<body>')
